6481864a357030730163692915f84c6d01c75379
kim gisu <kgs9843@naver.com>
2024-11-21 14:49:42 +0900

    feat: index.css 제거,app.css css 초기화, index.html에 구글 폰트 추가, 그룹 생성 페이지 제작

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { ThemeProvider } from 'styled-components';
-import { theme } from './theme';
-import Button from '@mui/material/Button';
-
-import './App.css'
+import MakingGroupPage from './pages/MakingGroupPage';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <ThemeProvider theme={theme}>
-        <div style={{ backgroundColor: theme.colors.background, padding: '20px' }}>
-          <h1>Hello, Vite + React + Styled Components + MUI!</h1>
-          <Button variant="contained" color="primary">MUI Button</Button>
-        </div>
-      </ThemeProvider>
+      <MakingGroupPage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
