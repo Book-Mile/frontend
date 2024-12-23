a4780e41ad9f4be9f2d6bd8bd2129f5e2e154aa8
0in <onezeroglgl@gmail.com>
2024-12-23 10:34:23 +0900

    design(index.css, App.jsx): 전체 레이아웃 설정

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,14 @@ import SignUp from './pages/SignUp';
 import { theme } from './theme';
 import { ThemeProvider } from 'styled-components';
 import CheckPointRecordPage from './pages/CheckPointRecordPage';
+{/*import NavBar from './components/NavBar'*/}
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
+      {/*<NavBar />*/}
+      <div className="main-content">
         <Routes>
           <Route path="/" element={<MakingGroupPage />} />
           <Route path="/makingGroup" element={<MakingGroupPage />} />
@@ -17,6 +20,7 @@ function App() {
           <Route path="/signUp" element={<SignUp />} />
           <Route path="/checkPointRecord" element={<CheckPointRecordPage />} />
         </Routes>
+        </div>
       </BrowserRouter>
     </ThemeProvider>
   );
