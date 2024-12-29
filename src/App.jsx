c33b6c7a94174babc6cec00c57a7787552a3f954
배현준 <hyunjunbae@naver.com>
2024-12-29 21:01:44 +0900

    design: 마이페이지 디자인 구현

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,29 @@ import { theme } from './theme';
 import { ThemeProvider } from 'styled-components';
 import CheckPointRecordPage from './pages/CheckPointRecordPage';
 import AnotherCheckPointRecordPage from './pages/AnotherCheckPointRecordPage';
+import MyPage from './pages/MyPage';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-      {/*<NavBar />*/}
-      <div className="main-content">
-        <Routes>
-          <Route path="/" element={<MakingGroupPage />} />
-          <Route path="/makingGroup" element={<MakingGroupPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signUp" element={<SignUp />} />
-          <Route path="/checkPointRecord" element={<CheckPointRecordPage />} />
-          <Route
-            path="/anothercheckPointRecord"
-            element={<AnotherCheckPointRecordPage />}
-          />
-        </Routes>
+        {/*<NavBar />*/}
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<MakingGroupPage />} />
+            <Route path="/makingGroup" element={<MakingGroupPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<SignUp />} />
+            <Route
+              path="/checkPointRecord"
+              element={<CheckPointRecordPage />}
+            />
+            <Route
+              path="/anothercheckPointRecord"
+              element={<AnotherCheckPointRecordPage />}
+            />
+            <Route path="/mypage" element={<MyPage />} />
+          </Routes>
         </div>
       </BrowserRouter>
     </ThemeProvider>
