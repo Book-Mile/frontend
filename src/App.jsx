4ea88e34fec4e3812ff2e2b809cf582db1eb43e7
0in <onezeroglgl@gmail.com>
2024-12-29 21:34:26 +0900

    fix: NavBar 변경 및 폰트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,29 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MakingGroupPage from './pages/MakingGroupPage';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
-import { theme } from './theme';
-import { ThemeProvider } from 'styled-components';
+import { theme } from './theme';  
+import { ThemeProvider } from 'styled-components'; 
 import CheckPointRecordPage from './pages/CheckPointRecordPage';
 import AnotherCheckPointRecordPage from './pages/AnotherCheckPointRecordPage';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}> 
       <BrowserRouter>
-      <NavBar />
-      <div className="main-content">
-        <Routes>
-          <Route path="/" element={<MakingGroupPage />} />
-          <Route path="/makingGroup" element={<MakingGroupPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signUp" element={<SignUp />} />
-          <Route path="/checkPointRecord" element={<CheckPointRecordPage />} />
-          <Route
-            path="/anothercheckPointRecord"
-            element={<AnotherCheckPointRecordPage />}
-          />
-        </Routes>
+        <NavBar />
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<MakingGroupPage />} />
+            <Route path="/makingGroup" element={<MakingGroupPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signUp" element={<SignUp />} />
+            <Route path="/checkPointRecord" element={<CheckPointRecordPage />} />
+            <Route
+              path="/anothercheckPointRecord"
+              element={<AnotherCheckPointRecordPage />}
+            />
+          </Routes>
         </div>
       </BrowserRouter>
     </ThemeProvider>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import NotifBell from './icons/NotifBell';
-import LoginButton from './LGButton/LGButton'
-import ModalButtonOk from './modalButton/ModalButtonOk'
-
+import { Link } from 'react-router-dom';
 
 const MainContainer = styled.div`
   overflow: hidden;
@@ -11,50 +8,72 @@ const MainContainer = styled.div`
   align-items: center;
   justify-content: space-between;
   flex-wrap: nowrap;
-  padding: 16px 80px;
-  background: ${props => props.theme.colors.background};
+  padding: 16px 100px;
+  background: white;
 
   * {
     box-sizing: border-box;
   }
 `;
 
-const LogoContainer = styled.div`
-  font-weight: bold; 
+const LogoContainer = styled(Link)`
+  text-decoration: none;
+  font-weight: bold;
 `;
 
-const NotifiContainer = styled.div`
+const LoginContainer = styled.div`
   display: flex;
   align-items: center;
-  height: 40px;
-  gap: 24px;
+  font-weight: 500;
+  gap: 20px;
 `;
 
-const ProfileImage = styled.div`
-  width: 40px;
-  height: 40px;
-  background-image: url(images/profile.png);
-  background-size: cover; 
-  background-position: center; 
-  border-radius: 50%; 
-  margin-right: 8px;
+const LoginText = styled(Link)`
+  color: ${props => props.theme.colors.main};
+  font-size: 16px;
+  cursor: pointer; 
+  background: transparent;
+  border: none;
+  padding: 8px 16px;
+  text-decoration: none;
+  padding: 10px 30px;
+  border-radius: 30px; 
+
+  &:hover {
+    background-color: #f0f0f0; 
+  }
+`;
+
+const SignUpLink = styled(Link)`
+  background-color: ${props => props.theme.colors.main};
+  color: white;
+  border-radius: 30px;
+  padding: 8px 16px;
+  text-decoration: none;
+  display: inline-block;
   cursor: pointer;
-`;
+  padding: 10px 30px;
 
+  &:hover {
+    background-color: #961414;
+  }
+`;
 
 export default function NavBar() {
   return (
     <nav className='navbar'>
-    <MainContainer>
-      <LogoContainer>LOGO</LogoContainer>
-      <LoginButton text="로그인" width="7.5rem"/>
-      <ModalButtonOk/>
+      <MainContainer>
+        <LogoContainer to='/'>LOGO</LogoContainer>
+        <LoginContainer>
+          <LoginText to="/login">로그인</LoginText>
+          <SignUpLink to="/signup">회원가입</SignUpLink>
+        </LoginContainer>
       {//로그인 후 사용
       /*<NotifiContainer>
           <NotifBell/>
           <ProfileImage />
       </NotifiContainer>*/}
-    </MainContainer>
+      </MainContainer>
     </nav>
   );
 }
