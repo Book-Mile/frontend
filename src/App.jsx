7d40962373c1e107f1ce03ec94b738e418066b0a
kim gisu <kgs9843@naver.com>
2024-12-31 03:56:27 +0900

    feat: 그룹생성팝업창생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
-import { theme } from './theme';  
+import { theme } from './theme';
 
 import NavBar from './components/NavBar';
 import MakingGroupPage from './pages/MakingGroupPage';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
-import CheckPointRecordPage from './pages/CheckPointRecordPage';
-import AnotherCheckPointRecordPage from './pages/AnotherCheckPointRecordPage';
+import CheckPointRecordPage from './Dispose/CheckPointRecordPage';
+import AnotherCheckPointRecordPage from './Dispose/AnotherCheckPointRecordPage';
+import JoinGroupPopup from './components/popup/JoinGroupPopup/JoinGroupPop';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}> 
+    <ThemeProvider theme={theme}>
       <BrowserRouter>
         <NavBar />
         <div className="main-content">
@@ -21,11 +22,15 @@ function App() {
             <Route path="/makingGroup" element={<MakingGroupPage />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signUp" element={<SignUp />} />
-            <Route path="/checkPointRecord" element={<CheckPointRecordPage />} />
+            <Route
+              path="/checkPointRecord"
+              element={<CheckPointRecordPage />}
+            />
             <Route
               path="/anothercheckPointRecord"
               element={<AnotherCheckPointRecordPage />}
             />
+            <Route path="/joinGroupPopup" element={<JoinGroupPopup />} />
           </Routes>
         </div>
       </BrowserRouter>
---
+++ src/components/popup/JoinGroupPopup/JoinGroupPop.jsx
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react';
+import {
+  PopupContainer,
+  PopupInner,
+} from '../../../styled_components/popupStyle';
+import useClosePopupAnimation from '../../../hooks/useClosePopupAnimation';
+import styled from 'styled-components';
+import ModalButtonOk from '../../modalButton/ModalButtonOk';
+import ModalButtonCancel from '../../modalButton/ModalButtonCancel';
+import { joinGroupPopupData } from '../../../data/joinGroupPopupData';
+
+const PopUpInnerBox1 = styled.div`
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 60px 80px;
+  gap: 50px;
+
+  width: 30%;
+  height: auto;
+
+  background: #ffffff;
+  border-radius: 20px;
+`;
+const Title = styled.div`
+  /* 그룹 정보 */
+
+  font-family: ${(props) => props.theme.font.main};
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 35px;
+  /* identical to box height */
+
+  color: #ab0909;
+
+  /* Inside auto layout */
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+const Content = styled.div`
+  font-family: ${(props) => props.theme.font.main};
+  font-size: 1rem;
+  line-height: 35px;
+  color: black;
+  .book-title {
+    font-weight: 800;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+const JoinGroupPopup = ({ onClose = false }) => {
+  const [isClosing, setIsClosing] = useState(false); // 닫힘 상태 관리
+
+  const handleClose = () => {
+    setIsClosing(true); // 닫히는 애니메이션 시작
+  };
+
+  useClosePopupAnimation(isClosing, onClose);
+
+  return (
+    <PopupContainer isClosing={isClosing}>
+      <PopupInner isClosing={isClosing}>
+        <PopUpInnerBox1>
+          <Title>그룹 정보</Title>
+          <Content>
+            <span className="book-title">"{joinGroupPopupData.title}"</span>에
+            참여하시겠습니까? <br /> 방식 : {joinGroupPopupData.subject}
+          </Content>
+          <ButtonContainer>
+            <ModalButtonOk width="150px" />
+            <ModalButtonCancel width="150px" onClick={handleClose} />
+          </ButtonContainer>
+        </PopUpInnerBox1>
+      </PopupInner>
+    </PopupContainer>
+  );
+};
+
+export default JoinGroupPopup;
