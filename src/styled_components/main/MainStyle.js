4a3d7c62c264f4758e1ee3290d2a6b7144cfe40d
0in <onezeroglgl@gmail.com>
2025-01-15 03:55:54 +0900

    refactor: 메인 페이지 리팩토링 및 컴포넌트화

---
+++ src/styled_components/main/MainStyle.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const MainWrapper = styled.main`
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  background-color: #f9f9f9;
+`;
+
+export const MainContent = styled.section`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 1155px;
+  height: 100vh;
+  margin: 0 auto;
+  text-align: left;
+  z-index: 2;
+
+  @media screen and (min-width: 1920px) {
+    padding: 0 10%;
+  }
+`;
+
+export const Detail = styled.div`
+  width: 1200px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 120px;
+`;
