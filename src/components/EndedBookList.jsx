579a4a253eda27a24a1c262db32a283613b28300
baebaej <korea024504@gmail.com>
2025-03-04 01:59:40 +0900

    feat: 마이페이지 책 클릭시 api연동

--- src/components/EndedBookList.jsx
+++ src/components/EndedBookList.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 /* eslint-disable react/prop-types */
 import ModeTag from '/src/components/modeTag/ModeTag.jsx';
 import { getUserGroups } from '../api/Pages/MyPageRequest.jsx';
+import { useNavigate } from 'react-router-dom';
 
 function Override({ groupName, groupKing, mode }) {
   return (
@@ -79,13 +80,26 @@ const UserImg = styled.div`
         : '0'}; /* 첫 번째는 0, 두 번째는 18px, 세 번째는 36px */
 `;
 
-function Book({ img, bookname, writer, groupMode, groupKing, groupName }) {
+function Book({
+  img,
+  bookname,
+  writer,
+  groupMode,
+  groupKing,
+  groupName,
+  groupId,
+}) {
+  const navigate = useNavigate();
+
   const [ishover, setHover] = useState(false);
   const handleHover = () => {
     setHover(!ishover);
   };
+  const handleClickBook = () => {
+    navigate(`/bookprogress?groupId=${groupId}`);
+  };
   return (
-    <ColFrame>
+    <ColFrame onClick={handleClickBook}>
       <BookImgFrame onMouseEnter={handleHover} onMouseLeave={handleHover}>
         <img
           src={img}
@@ -104,73 +118,6 @@ function Book({ img, bookname, writer, groupMode, groupKing, groupName }) {
     </ColFrame>
   );
 }
-
-// const books = [
-//   {
-//     img: '/src/assets/bookImg4.png',
-//     bookname: '채식주의자',
-//     writer: '한강',
-//     groupMode: 'speed',
-//     groupName: '베르테르 뮤지컬 예습',
-//     groupKing: '배현준',
-//   },
-//   {
-//     img: '/src/assets/bookImg2.png',
-//     bookname: '도둑 맞은 집중력',
-//     writer: '요한 하리',
-//     groupMode: 'speed',
-//     groupName: '최강컴공모임',
-//     groupKing: '김기수',
-//   },
-//   {
-//     img: '/src/assets/bookImg3.png',
-//     bookname: '영어로 문장 만들기 훈련',
-//     writer: '유은하',
-//     groupMode: 'page',
-//     groupName: '스프링이 뭔데요',
-//     groupKing: '한준서',
-//   },
-//   {
-//     img: '/src/assets/bookImg4.png',
-//     bookname: '채식주의자',
-//     writer: '한강',
-//     groupMode: 'speed',
-//     groupName: '문장의 정원',
-//     groupKing: '배현준',
-//   },
-//   {
-//     img: '/src/assets/bookImg4.png',
-//     bookname: '채식주의자',
-//     writer: '한강',
-//     groupMode: 'speed',
-//     groupName: '책모임 온누리',
-//     groupKing: '배현준',
-//   },
-//   {
-//     img: '/src/assets/bookImg.png',
-//     bookname: '좋은 사람 되려다 쉬운...',
-//     writer: '이남훈',
-//     groupMode: 'page',
-//     groupName: '같이 어휘력을 쌓읍시다',
-//     groupKing: '박영인',
-//   },
-//   {
-//     img: '/src/assets/bookImg4.png',
-//     bookname: '채식주의자',
-//     writer: '한강',
-//     groupMode: 'page',
-//     groupName: '책숲 동행',
-//     groupKing: '배현준',
-//   },
-//   {
-//     img: '/src/assets/bookImg4.png',
-//     bookname: '채식주의자',
-//     writer: '한강',
-//     groupMode: 'speed',
-//     groupName: '한 페이지 이야기',
-//     groupKing: '배현준',
-//   },
-// ];
 
 export default function EndedBookList() {
   const [responseData, setResponseData] = useState([]);
@@ -197,6 +144,7 @@ export default function EndedBookList() {
               groupMode={data.groupMode}
               groupName={data.groupName}
               groupKing={data.groupKing}
+              groupId={data.groupId}
             />
           ))}
         </ListRowFrame>
@@ -247,6 +195,7 @@ const BookImgFrame = styled.div`
   z-index: 1;
   position: relative; /* 부모 요소를 기준으로 자식 요소 위치 설정 */
   box-sizing: border-box;
+  cursor: pointer;
 `;
 
 const OverrideFrame = styled.div`
