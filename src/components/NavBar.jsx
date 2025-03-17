170e6f4d00da27449e4ad90ad46f8e607b11689f
0in2 <onezeroglgl@gmail.com>
2025-03-17 09:48:37 +0900

    fix: NavBar 중복 코드 제거

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState, useRef } from 'react';
-import { useEffect, useState, useRef } from 'react';
 import styled from 'styled-components';
 import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
